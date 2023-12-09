9aa10d4ed62378a4b84bf607bf80a3591f1bc699
adrianavinicki <adriana.vinicki@gmail.com>
2023-12-09 19:09:47 -0300

    push de projects

---
+++ src/app/components/data.js
@@ -0,0 +1,18 @@
+export const projects = [
+  {
+    title: "Wonder Toys",
+    subtitle: "a children's DREAM",
+    description:
+      "E-commerce website for children's toys where shopping becomes a game",
+    image: "./project-1.png",
+    link: "https://wondertoys.up.railway.app/",
+  },
+  {
+    title: "Strings And Keys",
+    subtitle: "a door to CREATION",
+    description:
+      "E-commerce to buy musical instruments creators of the best music",
+    image: "./project-2.png",
+    link: "https://stringsandkeys.up.railway.app/",
+  },
+];

4acb1e3e7d01359377e8a53ce44ee9495a17c78a
adrianavinicki <adriana.vinicki@gmail.com>
2023-12-10 19:40:28 -0300

    push de skills testimonials y contact, mas data actualizada

--- src/app/components/Skills.js
+++ src/app/components/Skills.js
@@ -9,29 +9,25 @@ export default function Skills() {
         <div className="text-center mb-20">
           <ChipIcon className="w-10 inline-block mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
-            Habilidades y Tecnologías
+            Skills and Technologies
           </h1>
-          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
-            ipsa delectus eum quo voluptas aspernatur accusantium distinctio
-            possimus est.
+          <p className="text-base leading-relaxed xl:w-2/3 lg:w-2/3 mx-auto">
+            🌟 Why Me?: Versatility: I bring a rich blend of experience from the
+            worlds of finance, business, and now technology. Problem-Solver: I
+            thrive in complex problem-solving scenarios, whether it's optimizing
+            financial operations or developing software solutions. Team Player:
+            Collaboration is at the core of my work ethic, and I love working
+            with diverse teams to achieve common goals.
           </p>
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
-          {skills.map((s) => (
-            <div className="flex relative">
-              <img
-                alt="gallery"
-                className="absolute inset-0 w-full h-full object-scale-down object-center"
-                src={s.image}
-              />
-              <div key={s} className="p-2 sm:w-1/2 w-full">
-                <div className="bg-gray-800 rounded flex p-4 h-full items-center">
-                  <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
-                  <span className="title-font font-medium text-white">
-                    {s.skill}
-                  </span>
-                </div>
+          {skills.map((skill) => (
+            <div key={skill} className="p-2 sm:w-1/2 w-full">
+              <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+                <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+                <span className="title-font font-medium text-white">
+                  {skill}
+                </span>
               </div>
             </div>
           ))}
--- src/app/components/data.js
+++ src/app/components/data.js
@@ -18,72 +18,67 @@ export const projects = [
 ];
 
 export const skills = [
+  "Javascript",
+  "Finance",
+  "NodeJs",
+  "Team Building",
+  "Express",
+  "Leadership",
+  "PostgreSQL",
+  "Decision-Making",
+  "Sequelize",
+  "Strategic-Thinking",
+  "React",
+  "Project Planning",
+  "Redux",
+  "Accounting",
+  "HTML",
+  "CSS",
+  "Git",
+  "GitHub",
+];
+
+export const testimonials = [
   {
-    skill: "Javascript",
-    image: "",
-  },
-  {
-    skill: "NodeJs",
-    image: "",
-  },
-  {
-    skill: "Express",
-    image: "",
-  },
-  {
-    skill: "PostgreSQL",
-    image: "",
-  },
-  {
-    skill: "Sequelize",
-    image: "",
-  },
-  {
-    skill: "React",
-    image: "",
-  },
-  {
-    skill: "Redux",
-    image: "",
-  },
-  {
-    skill: "HTML",
-    image: "",
-  },
-  {
-    skill: "CSS",
-    image: "",
-  },
-  {
-    skill: "Git",
-    image: "",
-  },
-  {
-    skill: "GitHub",
-    image: "",
-  },
-  {
-    skill: "Finance",
-    image: "",
-  },
-  {
-    skill: "Team Building",
-    image: "",
-  },
-  {
-    skill: "Leadership",
-    image: "",
-  },
-  {
-    skill: "Decision-Making",
-    image: "",
-  },
-  {
-    skill: "Strategic-Thinking",
+    quote:
+      "Adriana is an excellent professional with extensive experience in Administration and Finance department, obtained in first-class multinationals. It's a great group leader, oriented to get results, adding value and continuous improvement of processes.",
     //image: "",
+    name: "Daniel Puggioni",
+    company: "ACS Manager - LATAM at Amway",
   },
   {
-    skill: "Project Planning",
-    // image: "",
+    quote:
+      "Adriana is a competent F&A professional, has a balanced position while pursuing the strategic and budget goals, team worker and skilled to work with people. Result oriented. I´m proud to have had Adriana in my team.",
+    //image: "",
+    name: "Danilo Saicali",
+    company: "Partner at Santa Apolonia Particip.Ltda",
+  },
+  {
+    quote:
+      "Adriana is a seasoned executive with a high level of technical expertise in her field along with a detailed attention to the human factor of all work interactions. These traits make of Adriana a strong team leader and a great peer to work with and learn from. I certainly look forward to have the opportunity to work with her in the future.",
+    //image: "",
+    name: "Norma Treviño",
+    company: "Executive Coach - ICF ACC - Talent Management",
+  },
+  {
+    quote:
+      "He trabajado con Adriana muchos años, en los cueles hemos hecho un excelente equipo de trabajo. Adriana es una profesional con un alto seniority. Gran conocimiento de la función y tarea Adm/Financiera, marcado perfil para el trabajo en corporaciones internacionales, experiencia, manejo de situaciones conflictivas, adaptación a los cambios y al uso de diferentes herramientas ... en fin, mucho de lo que soy como profesional hoy se lo debo a Adriana.Y no solo eso, sino que también contribuyo a formar en mi una fuerte base (técnica, de razonamiento) que me sirvió para destacarme en futuros entornos muy competitivos. Una profesional con un fuerte perfil Administrativo Financiero, pero totalmente volcada a la obtención de resultados y a la mejora en la performance de la organización.",
+    //image: "",
+    name: "Roman Jasidakis",
+    company: "Nuvo Cosméticos",
+  },
+  {
+    quote:
+      "He compartido con Adriana durante más de 15 años el Comité de la Cámara Argentina de Venta Directa, en la cual hemos trabajado sobre proyectos correspondientes a la industria, y compartido información de alto valor apar el cuidado de las Empresas que conforman la Cámara, considerando que su aportes y trabajos han podido ser aplicados en su totalidad. En el último año, tuve la oportunidad de compartir, desarrollar e implementar el sistema de información, planning, prsupuesto y control Coprorativo ne Essen Alumninio S.A., siendo Adriana la Project Leader del proceso que se llevo a cabo en tiempos verdaderamente muy cortos. Recomiendo a Adriana, no sólo por sus conocimientos, practicidad y efectividad, sino además por sus conocimientos de la Venta Directa y sobre todo por su calidad personal, siendo ejemplo de honestidad, integridad y valores humanos.",
+    //image: "",
+    name: "Carlos Fidanza",
+    company: "Swiss Just Latinoamerica",
+  },
+  {
+    quote:
+      "I worked with Adriana for many years and she is a great resource with a solid knowledge of her job, and is always willing to help others. Adriana works with passion, is reliable and a team player. It was a pleasure to work with her!",
+    //image: "",
+    name: "Hilda Bovolon",
+    company: "Direct Selling Expert/Consultant, Business, Marketing & Sales",
   },
 ];
